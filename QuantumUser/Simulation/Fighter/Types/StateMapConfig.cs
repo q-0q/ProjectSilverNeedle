9b18d887601fe49c4b96270586eb9a6c328b980e
q-0q <jrwthrs@gmail.com>
2025-03-02 11:38:40 -0500

    deleting character class

---
+++ QuantumUser/Simulation/Fighter/Types/StateMapConfig.cs
@@ -0,0 +1,26 @@
+using Photon.Deterministic;
+using Quantum.Types;
+using Quantum.Types.Collision;
+
+namespace Quantum.StateMap
+{
+    public class StateMapConfig
+    {
+        public StateMap<FighterAnimation> FighterAnimation;
+        public StateMap<int> Duration;
+        public StateMap<SectionGroup<CollisionBoxCollection>> HurtboxCollectionSectionGroup;
+        public StateMap<SectionGroup<PlayerFSM.HurtType>> HurtTypeSectionGroup;
+        public StateMap<SectionGroup<Hit>> HitSectionGroup;
+        public StateMap<CollisionBox> Pushbox;
+        public StateMap<SectionGroup<FP>> MovementSectionGroup;
+        public StateMap<SectionGroup<bool>> AllowCrossupSectionGroup;
+        public StateMap<SectionGroup<Trajectory>> TrajectorySectionGroup;
+        // public StateMap<InputSystem.InputType> InputTypes;
+        // public StateMap<int> CommandDirection;
+        public StateMap<int> CancellableAfter;
+        public StateMap<bool> WhiffCancellable;
+        public StateMap<int> FireReceiverFinishAfter;
+        public StateMap<SectionGroup<FPVector2>> AttachPositionSectionGroup;
+        public StateMap<int> InvulnerableBefore;
+    }
+}
