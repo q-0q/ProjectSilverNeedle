76fdb8b3614f8fdf0a8e942bcc03f2583bfe010d
q-0q <jrwthrs@gmail.com>
2025-03-18 09:41:43 -0400

    movelist scrolling with dynamic sizing

--- QuantumUser/View/GameMenu/GameMenu.cs
+++ QuantumUser/View/GameMenu/GameMenu.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Quantum;
 using TMPro;
 using UnityEngine;
+using Input = UnityEngine.Input;
 
 public class GameMenu : MonoBehaviour
 {
@@ -12,6 +14,7 @@ public class GameMenu : MonoBehaviour
     private GameObject _tabList;
     private GameMenuTab _defaultTab;
     private GameMenuTab _currentTab;
+    public GameObject MoveListEntryPrefab;
 
     
     // Start is called before the first frame update
@@ -21,6 +24,9 @@ public class GameMenu : MonoBehaviour
         _currentTab = null;
         _canvas = transform.Find("MainCanvas").GetComponent<Canvas>();
         _tabList = transform.Find("MainCanvas").Find("MainPanel").Find("TabList").gameObject;
+        
+        QuantumEvent.Subscribe(listener: this, handler: (EventGameFinishLoading e) => PopulateMoveList());
+
     }
 
     private void Start()
@@ -61,5 +67,12 @@ public class GameMenu : MonoBehaviour
         tab.gameObject.SetActive(true);
         _currentTab = tab;
         transform.Find("MainCanvas").Find("MainPanel").Find("ContentPanel").Find("CurrentTabTitle").GetComponent<TextMeshProUGUI>().text = tab.TabName;
+    }
+
+    private void PopulateMoveList()
+    {
+        int playerId = 0; //todo
+        
+        
     }
 }
