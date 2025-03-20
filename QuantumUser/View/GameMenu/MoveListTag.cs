de9f2f23a60b565039e2de7c23baaa2cd5114c2f
q-0q <jrwthrs@gmail.com>
2025-03-20 17:48:23 -0400

    started on tags but stopping

---
+++ QuantumUser/View/GameMenu/MoveListTag.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveListTag : MonoBehaviour
+{
+    public enum Type
+    {
+        Launches,
+        GroundBounces,
+        WallBounces,
+        HardKnockdown,
+        Low,
+        High,
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
