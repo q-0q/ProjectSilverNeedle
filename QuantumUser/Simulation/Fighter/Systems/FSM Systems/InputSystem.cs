ff02aea35e17f8ad75602702b6f077e0985b01b5
q-0q <jrwthrs@gmail.com>
2025-03-24 22:26:34 -0400

    rm airdash: todo min height for ait actions

--- QuantumUser/Simulation/Fighter/Systems/FSM Systems/InputSystem.cs
+++ QuantumUser/Simulation/Fighter/Systems/FSM Systems/InputSystem.cs
@@ -196,6 +196,7 @@ namespace Quantum
             }
             else if (fsm.InputIsBuffered(InputType.Dash, f, fsm.EntityRef))
             {
+                Debug.Log(InheritableEnum.InheritableEnum.GetFieldNameByValue(fsm.Fsm.State(), fsm.StateType));
                 fsm.Fsm.Fire(PlayerFSM.PlayerTrigger.Dash, param);
             }
         }
