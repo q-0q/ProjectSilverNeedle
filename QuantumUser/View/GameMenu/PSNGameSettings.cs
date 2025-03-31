a46533e6f2eecf502e19abaab8ff42af6d216868
q-0q <jrwthrs@gmail.com>
2025-03-30 20:12:58 -0400

    30 minute settings menu

---
+++ QuantumUser/View/GameMenu/PSNGameSettings.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Quantum.Types.Collision;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class PSNGameSettings : MonoBehaviour
+{
+    private Light _light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = FindObjectOfType<Light>();
+    }
+
+    public void ToggleCollisionBoxes()
+    {
+        CollisionBoxViewer.OnCollisionBoxesToggled?.Invoke();
+    }
+    
+    public void ToggleFrameMeter()
+    {
+        FrameMeterReporter.OnFrameMeterToggled?.Invoke();
+    }
+    
+    public void ToggleShadows()
+    {
+        _light.shadows = _light.shadows == LightShadows.Hard ? LightShadows.None : LightShadows.Hard;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
