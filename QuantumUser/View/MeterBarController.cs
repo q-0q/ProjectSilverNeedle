bf846ebfa601639f9464b0fb879a5b6843f74e83
q-0q <jrwthrs@gmail.com>
2025-04-09 19:01:32 -0400

    meter bar and stuff

--- QuantumUser/View/MeterBarController.cs
+++ QuantumUser/View/MeterBarController.cs
@@ -13,8 +13,8 @@ public class MeterBarController : MonoBehaviour
 {
     public static MeterBarController Instance;
 
-    public RectTransform Player0HealthBar;
-    public RectTransform Player1HealthBar;
+    public RectTransform Player0MeterBar;
+    public RectTransform Player1MeterBar;
     
     public RectTransform Player0ComboBar;
     public RectTransform Player1ComboBar;
@@ -32,16 +32,16 @@ public class MeterBarController : MonoBehaviour
     {
         Instance = this;
         
-        Player0HealthBar = transform.Find("HealthBar0").Find("Holder").Find("Red").GetComponent<RectTransform>();
-        Player1HealthBar = transform.Find("HealthBar1").Find("Holder").Find("Red").GetComponent<RectTransform>();
+        Player0MeterBar = transform.Find("MeterBar0").Find("Holder").Find("Red").GetComponent<RectTransform>();
+        Player1MeterBar = transform.Find("MeterBar1").Find("Holder").Find("Red").GetComponent<RectTransform>();
 
-        _0defaultLocalPosition = transform.Find("HealthBar0").Find("Holder").localPosition;
-        _1defaultLocalPosition = transform.Find("HealthBar1").Find("Holder").localPosition;
+        _0defaultLocalPosition = transform.Find("MeterBar0").Find("Holder").localPosition;
+        _1defaultLocalPosition = transform.Find("MeterBar1").Find("Holder").localPosition;
         
-        Player0ComboBar = transform.Find("HealthBar0").Find("Holder").Find("Combo").GetComponent<RectTransform>();
-        Player1ComboBar = transform.Find("HealthBar1").Find("Holder").Find("Combo").GetComponent<RectTransform>();
+        Player0ComboBar = transform.Find("MeterBar0").Find("Holder").Find("Combo").GetComponent<RectTransform>();
+        Player1ComboBar = transform.Find("MeterBar1").Find("Holder").Find("Combo").GetComponent<RectTransform>();
         
-        _maxBarLength = Player0HealthBar.localScale.x;
+        _maxBarLength = Player0MeterBar.localScale.x;
 
     }
 
@@ -57,7 +57,7 @@ public class MeterBarController : MonoBehaviour
         if (playerId == 1) prev1Meter = meter;
 
         
-        RectTransform healthRectTransform = playerId == 0 ? Player0HealthBar : Player1HealthBar;
+        RectTransform healthRectTransform = playerId == 0 ? Player0MeterBar : Player1MeterBar;
         RectTransform comboRectTransform = playerId == 0 ? Player0ComboBar : Player1ComboBar;
         
         
@@ -73,14 +73,14 @@ public class MeterBarController : MonoBehaviour
 
     private float GetWidth(float health)
     {
-        var maxBarLength = (health / 500f) * _maxBarLength;
+        var maxBarLength = (health / 100f) * _maxBarLength;
         return Mathf.Max(maxBarLength, 0);
     }
     
     
     private void Vibrate(int player, float strength, float duration, int vibrato)
     {
-        Transform t = player == 0 ? transform.Find("HealthBar0").Find("Holder") : transform.Find("HealthBar1").Find("Holder");
+        Transform t = player == 0 ? transform.Find("MeterBar0").Find("Holder") : transform.Find("MeterBar1").Find("Holder");
         t.localPosition = player == 0 ? _0defaultLocalPosition : _1defaultLocalPosition;
         t.DOShakePosition(duration, strength, vibrato, 90f, false, true, ShakeRandomnessMode.Full);
     }
