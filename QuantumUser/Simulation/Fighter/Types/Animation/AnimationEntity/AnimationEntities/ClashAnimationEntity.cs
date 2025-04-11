7bf9e8663bcd9f0caf5a1f9f954ed25e97e91916
q-0q <jrwthrs@gmail.com>
2025-04-10 22:46:08 -0400

    late night clash tweak

--- QuantumUser/Simulation/Fighter/Types/Animation/AnimationEntity/AnimationEntities/ClashAnimationEntity.cs
+++ QuantumUser/Simulation/Fighter/Types/Animation/AnimationEntity/AnimationEntities/ClashAnimationEntity.cs
@@ -15,8 +15,8 @@ namespace Quantum.Types
                 Sections = new List<Tuple<int, int>>()
                 {
                     new(5, 0),
-                    new(13, 1),
-                    new(3, 2),
+                    new(11, 1),
+                    new(6, 2),
                 }
             };
         }
