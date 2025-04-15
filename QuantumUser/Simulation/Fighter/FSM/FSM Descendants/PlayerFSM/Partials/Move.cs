d900ae0fad6dba0fa1b27c590b070c41f713410b
q-0q <jrwthrs@gmail.com>
2025-04-14 21:34:47 -0400

    add hit visual hit position

--- QuantumUser/Simulation/Fighter/FSM/FSM Descendants/PlayerFSM/Partials/Move.cs
+++ QuantumUser/Simulation/Fighter/FSM/FSM Descendants/PlayerFSM/Partials/Move.cs
@@ -82,36 +82,5 @@ namespace Quantum
 
             ApplyFlippedMovement(f, v, entityRef, true);
         }
-        
-        private void OnEnterThrowTech(TriggerParams? triggerParams)
-        {
-            if (triggerParams is null) return;
-            var frameParam = (FrameParam)triggerParams;
-
-            var distance = _throwTechPushback;
-            if (IsOnLeft(frameParam.f, EntityRef)) distance *= FP.Minus_1;
-            StartPushback(frameParam.f, distance);
-            Util.StartDramatic(frameParam.f, EntityRef, 35);
-
-            // only make the tech animation sprite for player 0
-            if (Util.GetPlayerId(frameParam.f, EntityRef) != 0) return;
-
-            frameParam.f.Unsafe.TryGetPointer<Transform3D>(EntityRef, out var transform3D);
-            frameParam.f.Unsafe.TryGetPointer<Transform3D>(Util.GetOtherPlayer(frameParam.f, EntityRef),
-                out var opponentTransform3D);
-
-            FPVector2 attachPos = GetFirstKinematicsAttachPosition(frameParam.f, EntityRef);
-            FPVector2 opponentAttachPos =
-                GetFirstKinematicsAttachPosition(frameParam.f, Util.GetOtherPlayer(frameParam.f, EntityRef));
-
-            FPVector2 pos = attachPos + transform3D->Position.XY;
-            FPVector2 opponentPos = opponentAttachPos + opponentTransform3D->Position.XY;
-
-            FPVector2 avgPos = (pos + opponentPos) * FP._0_50;
-
-            AnimationEntitySystem.Create(frameParam.f, AnimationEntities.AnimationEntityEnum.Tech,
-                avgPos, 0,
-                false);
-        }
     }
 }
